58d5eff35842eaa074be917f22ac83e5ba89b085
Tumerkan Karabel <TumerkanKarabel@example.com>
2024-08-01 19:51:19 +0300

    Add movies app

---
+++ filmler_app/lib/data/entity/filmler_cevap.dart
@@ -0,0 +1,19 @@
+import 'package:filmler_app/data/entity/filmler.dart';
+
+class FilmlerCevap {
+  List<Filmler> filmler;
+  int success;
+
+  FilmlerCevap({required this.filmler, required this.success});
+
+  factory FilmlerCevap.fromJson(Map<String, dynamic> json) {
+    var jsonArray = json["filmler"] as List;
+    var success = json["success"] as int;
+
+    var filmler = jsonArray
+        .map((jsonArrayNesnesi) => Filmler.fromJson(jsonArrayNesnesi))
+        .toList();
+
+    return FilmlerCevap(filmler: filmler, success: success);
+  }
+}
